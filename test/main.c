d4ca3acd5bb87c888b0bdbba0915a999e327f1ae
Huaqi Fang <578567190@qq.com>
2024-06-25 15:23:28 +0800

    update test validate code and tested on nuclei evaluation soc
    It just works
    
    Nuclei SDK Build Time: Jun 25 2024, 15:15:02
    Download Mode: ILM
    CPU Frequency 50327060 Hz
    CPU HartID: 0
    Start to do flash loader test at SPI base address 0x10014000
    The current Flash ID is 0x1840ef
    Erase success
    Write success
    read success
    Test pass
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- test/main.c
+++ test/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define DEBUG_INFO          (0)
+#define DEBUG_INFO          (1)
 
 /*==== Loader ====*/
 #define ERASE_CMD           (1)
@@ -9,6 +9,7 @@
 #define READ_CMD            (3)
 #define PROBE_CMD           (4)
 
+// TODO Modify the following macro as your test requirements
 #define TEST_ERASE_SIZE     (0x10000)
 #define TEST_OFFSET         (0x00)
 #define TEST_COUNT          (512)
@@ -19,7 +20,8 @@ uint8_t read_buf[TEST_COUNT] = {0};
 void data_init(uint8_t* buf, uint32_t len)
 {
     for (int i = 0;i < len;i++) {
-        buf[i] = i + 1;
+        // TODO you can random it
+        buf[i] = i + 2;
     }
 }
 
@@ -33,6 +35,11 @@ int memory_compare(uint8_t* src, uint8_t* dst, uint32_t len)
     return 0;
 }
 
+
+// TODO Change SPI_BASE to your real base address
+
+#define SPI_BASE 0x10014000
+
 extern int loader_main(uint32_t cs, uint32_t *spi_base, uint32_t params1, uint32_t params2, uint32_t params3);
 
 void main(void)
@@ -42,10 +49,14 @@ void main(void)
 
     /* flash loader in/out params */
     uint32_t cs = 0x00;
-    uint32_t *spi_base = (uint32_t*)0x10014000;
+    uint32_t *spi_base = (uint32_t*)SPI_BASE;
     uint32_t params1 = 0x00;
     uint32_t params2 = 0x00;
     uint32_t params3 = 0x00;
+
+    #if DEBUG_INFO
+    printf("Start to do flash loader test at SPI base address 0x%x\r\n", spi_base);
+    #endif
 
     /* data init */
     data_init(write_buf, TEST_COUNT);
